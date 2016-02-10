0a91899f5eb4dd07799e2c4ffbc90d627406054e
asherfsullivan <asherfolsomsullivan@gmail.com>
2016-02-10 13:15:28 -0500

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "  
+end
+
+def valid_move?(board, position)
+  if position.to_i.between?(1,9) && board[position.to_i-1] == " "
+    true
+  else
+    false
+  end
+end
+
+def move(board, position, player = "X")
+  board[position.to_i-1] = player
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  if valid_move?(board, input)
+    move(board, input, "X")
+  else
+    turn(board)
+  end
+  display_board(board)
+end
