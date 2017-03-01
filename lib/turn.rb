41c75fa69bd8a7b24f273e2085ba499dea09d633
Jennifer Haddock <jentx123@hotmail.com>
2017-03-01 04:03:12 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,13 +37,13 @@ def turn(board)
   index = 0
   until play_made == true
     puts "Please enter 1-9:"
-    input = gets.strip 
-    input_to_index(input.to_i)
-    index = input.to_i - 1 
+    input = gets.strip
+#    input_to_index(input.to_i)
+    index = input_to_index(input.to_i)
     if valid_move?(board, index) == true &&  position_taken?(board, index) == false
       move(board, index)
       display_board(board)
       play_made = true
     end
-  end    
+  end
 end
