e637b10034e47e72a3531a57c410b1030403e6bb
ctab33 <ctab33@me.com>
2017-05-03 13:11:53 -0400

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,17 +17,24 @@ def move(array, index, character="X")
 end
 # code your #valid_move? method here
 def valid_move?(board, index)
-  indexMove = index.to_i
-  indexMove - 1
-  indexMove.between?(0, 8) && !position_taken?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
 end
-
-
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  board.include?("")
-  !board.include?(" ")
-  board.include?("X" || "O")
+  board[index] == "X" || board[index] == "O"
+end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    puts "Invalid Move"
+    display_board(board)
+    turn(board)
+  end
 end
