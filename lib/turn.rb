e900de3e83746f23f88a7ef029421b09568bb8e1
britt <britt.cumens@gmail.com>
2017-05-21 13:48:17 -0400

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,9 +35,10 @@ def move(board, index, character = "X")
   board[index] = character
 end
 
-def turn(input)
+def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   input_to_index(input)
-  
+  valid_move?(board, index)
+
 end
