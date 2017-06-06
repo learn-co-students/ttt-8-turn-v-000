d7878799298df6c9047482689fd8a1f3fc37aee0
Auden Robertson <audenrobertson@gmail.com>
2017-06-06 00:03:59 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,45 @@
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, input_to_index, value = "X")
+  puts board[input_to_index] = value
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board,index) == false
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(board, index) == true
+    move(board, index, value = "X")
+    display_board(board) 
+  else
+    turn(board)
+
+  end
+
+end
