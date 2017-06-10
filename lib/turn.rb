d3e7b77f41ade5bde9fd3617a6c20a1a603c8dae
Karen Berba <kberba@gmail.com>
2017-06-10 19:34:27 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index( user_input )
+  user_input = user_input.to_i
+  user_input -= 1
+end
+
+def valid_move?( board, index )
+  if position_taken?( board, index ) == true || !index.between?(0,9)
+    return false
+  else
+    return true
+  end
+end
+
+def move ( board, index, character = "X" )
+  board[index] = character
+end
