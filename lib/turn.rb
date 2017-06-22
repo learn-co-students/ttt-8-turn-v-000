e5a7135c08947ddcd0f1965eaee99b3e9f505e6b
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-06-22 00:47:55 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+# #DISPLAY_BOARD
+# Should accept a board as an argument and print out the current state of the board for the user.
+
+#VALID_MOVE?
+# Should accept a board and an index from the user and return true if the index is within the correct range of 0-8 and is currently unoccupied by an X or O token.
+
+# ask for input
+
+
+# get input
+
+
+
+# convert input to index
+
+#MOVE
+# This method should accept a board, an index from the user (which was converted from their raw input with input_to_index), and a token to mark that position with (you can give that argument a default value of 'X'––we're not worrying about whose turn it is yet). The method should set the correct index value of that position within the board equal to the token.
+
+# if index is valid
+#   make the move for index
+#   show the board
+# else
+#   ask for input again until you get a valid input
+# end
