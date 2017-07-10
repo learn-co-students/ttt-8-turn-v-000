47c4de61e5cba53cd912979fab2c96b326473ff8
Jen Poole <contact.vandepool@gmail.com>
2017-07-10 03:23:42 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,3 @@
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,36 +6,27 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def position_taken?(board, position)
-  if board[position] == " " || board[position] == "" || board[position] == nil
-    false
-  else
-    true
-  end
-end
-
 def valid_move?(board, position)
-  if position.between?(0,8) && !position_taken?(board, position)
+  if position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
     true
-  else
-    false
   end
-end
-
-def move(board, position, player="X")
-  board[position] = player
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.to_i # get user input (1-9)
-  position = input - 1 # convert user input number to an index (0-8)
-
-  if valid_move?(board, position)
-    move(board, position) # if move is valid, add player symbol to board array at position
-
-    display_board(board) # display board with player move
+  input = gets.strip
+  if valid_move?(board, input)
+    move(board, input)
   else
-    turn(board) # if move isn't valid, ask again for player move
+    turn(board) and return
   end
+  display_board(board)
+end
+
+def position_taken?(board, location)
+  board[location] != " "
+end
+
+def move(board, location, current_player = "X")
+  board[location.to_i-1] = current_player
 end
