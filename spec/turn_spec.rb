60657507fb63a57b57e16ef18bc706f7985e7bc9
alpbourne <alpbourne@gmail.com>
2017-08-17 20:26:25 +0000

    Done.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,5 +1,38 @@
 require_relative "../lib/turn.rb"
 
+board = ["   ","   ","   ","   "," x ","   ","   ","   ","   "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(value)
+  integer = value.to_i
+  index = integer - 1
+  return index
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    return false
+  elsif index < 0 || index > 8 || index == nil
+    return false
+  else
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "O" || board[index] == "X"
+  return true
+elsif board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+end
+end
+
 describe './lib/turn.rb' do
 
   describe '#display_board' do
