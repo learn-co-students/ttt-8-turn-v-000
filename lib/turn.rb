13d773a5698664e877c0433e8e5385f079d7e020
mstmari <michael.landrum.st.marie@gmail.com>
2017-10-26 16:19:52 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,48 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input_to_index
+  if
+    valid_move? == true
+    move(board, move, char = "X")
+  else
+    until move(board, move, char = "X") == valid_move?(board, index)
+      turn(board)
+  end
+ end
+end
+
+
+
+
+
+
+#board & display_board
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+#valid_move
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+  true
+else
+  false
+  end
+end
+
+
+# move method
+def move(board, move, char = "X")
+  board[move] = char
+end
+
+def input_to_index(number)
+  number.to_i - 1
+
+end
