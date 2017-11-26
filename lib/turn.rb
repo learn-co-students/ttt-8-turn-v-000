6536167b0ca249fc57faf3b73cf1cef17a8c1373
Kira Rice <kiramrice@gmail.com>
2017-11-26 02:50:07 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,47 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  input = user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  move = index.to_i - 1
+  if (position_taken?(board, index) || index.between?(0, 8) == false) # <- syntax error, needed parentheses to get tests passings (doh!)
+    return false
+  else
+    return true
+  end
+end
+
+def move(board, index, character="X")
+  board[index] = character
+  board
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+     move(board, index)
+     display_board(board)
+  else
+    turn(board) #turn method calls itself (recursion)
+  end
+end
+
+def position_taken? (board, index)
+  taken = nil
+  if (board[index] ==  " " || board[index] == " " || board[index] == nil)
+  taken = false
+  else
+  taken = true
+  end
+  taken
+end
