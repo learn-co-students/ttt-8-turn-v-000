235a3461b3c66456dbdae68eff0b4886de48b774
Kristen Leach <kristenleach24@gmail.com>
2017-11-26 23:14:40 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,9 +28,8 @@ def move(array, input, char = "X")
 end
 
 def turn(board)
-counter = 0 
-  until counter = 9
     puts "Please enter 1-9:"
-    counter += 1 
-  end
+    input = gets.strip
+    index = input_to_index(input)
+    move(board, index)
 end
