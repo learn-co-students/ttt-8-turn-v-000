a51ecd9e73bf72bd9c647182bc87c467ec356be4
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2017-11-29 04:40:02 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,52 @@
+# displays board current state [X]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# Asks user for input
+# 1] if move is valid, make move
+# 2] if move is NOT valid ask for input until move is valid
+def turn(board)
+  puts "Please enter 1-9:"
+  index = input_to_index(gets.strip)
+  if valid_move?(board, index)
+    move(board, index, char = "X")
+    puts "You chose position #{index + 1}"
+    display_board(board)
+  else
+    until valid_move?(board, index)
+      puts "Invalid Move, Please Choose Again (Must Be A Number 1-9)"
+      index = input_to_index(gets.strip)
+    end
+  end
+end
+
+# Ensures user does not input an empty string [X]
+def check_input?(board, index)
+  if board[index] == "" || board[index] == " "
+    return false
+  else
+    return true
+  end
+end
+
+# ensure move is valid
+# 1] index is empty (check_input) [X]
+# 2] index between 0-8 [X]
+def valid_move?(board, index)
+  !check_input?(board, index) && index.between?(0, 8)
+end
+
+# if move valid convert input to integer [X]
+def input_to_index(index)
+  index = index.to_i - 1
+end
+
+# Makes move (adds to index by reassign) [X]
+def move(board, index, char = "X")
+  board[index] = char
+end
