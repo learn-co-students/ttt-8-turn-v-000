1344a52079d03ab4bca85e1966449f9e6a5089a7
EPIC448 <samrey2018@gmail.com>
2017-12-01 18:09:11 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,69 @@
+# Define display_board that accepts a board and prints
+# out the current state.
+ def display_board(board)
+
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+end
+
+# code your #valid_move? method here
+ def valid_move?(board,index)
+   if !position_taken?(board, index)   && index.between?(0,8)
+      true
+
+   end
+ end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if (board[index] == " ")
+    false
+
+  elsif (board[index] == "X") || (board[index] == "O")
+    true
+
+  else
+    false
+
+ end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+#
+ def input_to_index(input)
+   #when ever you use a parameter in you method..MAKESURE to USE IT IN YOUR METHOD BODY. (NOT AS A "STRING")
+   input.to_i - 1
+
+ end
+
+ def move(array, index, value = "X")
+   array[index] = value
+
+ end
+
+
+# method turn
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+     move(board, index)
+     display_board(board)
+
+  else
+    turn(board)
+  end
+ end
