12b0847c499f47349db1f00ca8cc4077154ba2b7
Hanna Vanderloop <hvanderloop@gmail.com>
2017-12-03 00:22:29 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,4 @@
-board = ["O", "O", "O", "O", "O", "O", "O", "O", "O",]
-
+board = ["", "", "", "", "", "", "", "", ""]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -9,32 +8,30 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input = user_input.to_i
-  index = user_input - 1
+  user_input.to_i-1
+  index = user_input.to_i-1
 end
 
 #move
-def move(board, index, character='X')
-  board[index]=character
+def move(board, user_input,character="X")
+  board[user_input]=character
 end
 
 def valid_move?(board,index)
-  if position_taken?(board,index)
-
-  else index.between?(0,8)
+  if index.between?(0,8) && position_taken?(board, index) == false
+    true
   end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board,index_number)
- board[index_number] == " "
-board[index_number] != ""
-board[index_number] == "nil"
-board[index_number] == "X"
-board[index_number] == "X" || board[index_number] == "O"
+def position_taken?(board, index)
+  if (board[index]==" "||board[index]==""||board[index]==nil)
+    false
+  else (board[index]=="X"||board[index]=="O")
+    true
+  end
 end
 
-#turn
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.chomp
