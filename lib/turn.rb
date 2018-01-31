69a2e401101c363ab737af848edceea58feef353
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-01-31 16:11:41 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,13 @@
-def display_board(board) 
-  
-end 
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+   
+   puts " X | X | X " 
+   puts "-----------"
+   puts " X | O | O "
+   puts "-----------"
+   puts " X | O | X "
+   puts "-----------"
+   puts " O | X | X "
+   puts "-----------"
+   puts " O | X | O "
+   
+   end 
