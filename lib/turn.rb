9c9303bd2404be5bef0e046ecb326ef2b92bacf3
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-02-02 00:06:46 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,8 +40,15 @@ end
     turn(board)
   end 
     display_board(board)
- end 
+  end 
  
+ def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
  
- 
- 
+   
+  
