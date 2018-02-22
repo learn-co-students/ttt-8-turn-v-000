712d16507d2fa0f9eab364dc24aad9f598ab19c9
Jamal Gelin <gelin.jamal@gmail.com>
2018-02-22 17:29:52 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,33 +7,43 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+ end
+
+def move(board, user_input, char = "X")
+board[user_input] = char
+end
+
+def valid_move?(board, index)
+ !position_taken?(board, index) && index.between?(0, 8)
+end
+
 def position_taken?(board, index)
-+  if board[index] == "  " || board[index] == " " || board[index] == "" || board[index] == NIL
-+    false
-+  else
-+    TRUE
-+  end
-+end
-+def valid_move?(board, index)
-+  if position_taken?(board, index) == false && index.between?(0, 8)
-+    return TRUE
-+  else
-+    false
-+  end
-+end
-+
-+def move(board, input_to_index, players_char = "X")
-+  board[input_to_index] = players_char
-+  board
-+end
-+def input_to_index(user_input)
-+  user_input.to_i - 1
-+end
-+def turn(board)
-+  puts "Please enter 1-9:"
-+  user_selection = gets.strip
-+index = input_to_index(user_selection)
-+   valid_move?(board, index) ? move(board, index) : turn(board)
-+
-+  display_board(board)
-+end 
+board[index] != " "
+end
+
+def turn(board)
+    puts "Please enter 1-9:"
+input = gets.strip
+
+new_input = input_to_index(input)
+
+if valid_move?(board,new_input)
+
+  move(board, new_input)
+
+  display_board(board)
+else
+  turn(board)
+end
+end
