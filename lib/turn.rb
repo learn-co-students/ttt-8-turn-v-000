45465e5c879c5d4c2611f07c29b1e88108e58c4b
David Markel <davidgmarkel@gmail.com>
2018-03-07 19:13:50 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,7 +21,7 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  if board[index] == "" || board[index] == " " || board[index] == nil
+  if board[index] == "" || board[index] == " " 
     false
   elsif board[index] == "X" || board[index] ==  "O"
     true
