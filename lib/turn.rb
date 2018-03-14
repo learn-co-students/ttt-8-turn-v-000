2b8a45ca9eadaf388938840b1b449f04b2473aca
Igli Cekani <htnacekan@gmail.com>
2018-03-14 00:25:06 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -59,9 +59,11 @@ puts "Please enter 1-9:"
 user_input=gets
 field=user_input
 user_input=input_to_index(user_input)
+
 if valid_move?(board, index=user_input)==true
-  #board[index]=field
+  board[index]="X"
   move(board, index, input=field)
+  display_board(board)
 elsif valid_move?(board, index=user_input)==false
     while valid_move?(board, index=user_input)==false
       user_input=gets
