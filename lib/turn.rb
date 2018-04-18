785bc8773f51755852d8cca6da717f474267ee99
Dale Terrell <tragicid@gmail.com>
2018-04-18 16:16:11 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+def valid_move?(board, index)
+  index.between?(0, 8) && !(position_taken?(board, index))
+end 
+
+
+
+def position_taken?(board, index)
+ !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+end
+
+
+def input_to_index(move)
+  move.to_i-1
+end
+
+def move(array, index, value="X")
+  array[index] = value
+end
+
+
+
