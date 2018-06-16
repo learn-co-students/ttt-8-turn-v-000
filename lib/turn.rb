2c02fc85b24003c2500be8710626368d30cbb45d
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-16 00:15:41 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  separator = "-----------"
+  puts separator
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts separator
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1 
+end
+
+def position_taken?(board, index)
+  if 
+    board[index] == " " || "" || board[index] == nil
+    false
+  elsif
+    board[index] == "X" || board[index] == "O"
+    true 
+  end
+end
+
+def valid_move?(board, index)
+ if index.between?(0, 8) && !position_taken?(board, index) 
+   true
+ else 
+   false 
+  end
+end
