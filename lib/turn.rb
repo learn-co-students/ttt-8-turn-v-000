b82ffc4edda8ff9523ca6b233a363707c644a6fe
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-06-16 00:17:58 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   separator = "-----------"
@@ -22,6 +23,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
+ binding.pry
  if index.between?(0, 8) && !position_taken?(board, index) 
    true
  else 
