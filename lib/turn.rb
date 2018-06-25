701be77c5baa5c005b7ba90b5bbd81f9ea989d60
Jonathan Fernandez <jfern20@gmail.com>
2018-06-25 03:32:05 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,18 +8,8 @@ def display_board(board)
 end 
 
 def   valid_move? (board, index)
+      index.between?(0, 8) && board[index] ==" " || board[index] ==""
   
-  if board[index] ==" " || board[index] =="" 
-  true
-
-  
-   elsif board[index] == "X" || board[index] == "O"
-   false 
-   
-   elsif index < 0
-   false
-
-   end
 
 end
 
