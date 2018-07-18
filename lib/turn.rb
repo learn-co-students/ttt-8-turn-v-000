e569d121c5d1887bfb08d0b208408e330675b219
Heshie Brody <4hbrody@gmail.com>
2018-07-18 04:54:13 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,18 +1,41 @@
-def display_board(board)
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def turn(board)
+  puts "Please enter 1-9:"
+    index = input_to_index(gets.to_i)
+      while !valid_move?(board, index)
+        puts "Please enter 1-9:"
+        index = input_to_index(gets.to_i)
+      end
+      if valid_move?(board, index)
+          display_board(board)
+      end
 end
+
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def input_to_index(input)
-index = input.to_i - 1
+    index = input.to_i - 1
 end
 
 def valid_move?(board, index)
+    if index.between?(0, 8) && !position_taken?(board, index)
+      return true
+    end
+end
 
+def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    elsif board[index] == "X" || board[index] == "O"
+      return true
+    end
 end
 
 def move(board, index, player = "X")
-
+    board[index] = player
 end
