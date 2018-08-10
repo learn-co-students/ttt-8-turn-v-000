fc82184abf434377769aff73ddefc59eecfbcbc2
Amanda Murphy <helloamandamurphy@gmail.com>
2018-08-10 23:22:00 +0000

    Done.

---
+++ garbage_truck.rb
@@ -0,0 +1,21 @@
+def valid_move?(board,index)
+  if index < 0 || index > 8 || board[index] == "X" || board[index] == "O"
+    def valid_loop
+      
+  else (index == 0 || index > 0 || index < 8) && (board[index] == "" || board[index] == " " || board[index] == nil)
+    def move(array, index, value = "X")
+   array[index] = value
+ end
+  end
+end
+
+def valid_loop
+  if valid_move?(board,index) == false
+    puts 
+    
+    #Move
+ def move(array, index, value = "X")
+   array[index] = value
+ end
+ 
+ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
