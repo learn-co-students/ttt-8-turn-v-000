e5e792f6edae7b160d5fce10c298bcd72d77aa4b
veloblank <veloblank@gmail.com>
2018-09-10 01:48:14 -0400

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,54 +1,46 @@
-board = [" "," "," "," "," "," "," "," "," ",]
-
-def display_board(board)
-    print " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  index = input_to_index(input)
+  if !valid_move?(board, index)
+    turn(board)
+  end
+  if position_taken?(board, index)
+    turn(board)
+  else
+    move(board, index, char="X")
+  end
+  display_board(board)
 end
 
-print display_board(board)
 
-def input_to_index(user_input)
-  user_input.to_i - 1
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board, index)
-  if index.between?(1,9)
-    if !position_taken?(board, index)
-      true
-    end
+  if index >= 0 && index <= 9
+    !position_taken?(board, index)
+  else
+    false
   end
 
-  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def input_to_index(input)
+  selection = input.to_i - 1
 end
 
 def position_taken?(board, index)
-  if (board[index] == " " || board[index] == "" || board[index]== nil)
-    taken = false
-  else
-    taken = true
-  end
-taken
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-      input = gets.strip
-      index = input_to_index(input)
-      if valid_move?(board, index)
-        move(board, index)
-        display_board(board)
-      else
-        turn(board)
-      end
-end
-
-def position_taken?(board, index)
-  if (board[index] == " " || board[index] == "" || board[index]== nil)
-    taken = false
-  else
-    taken = true
+  if board[index] == "X" || board[index] == "O"
+    true
   end
 end
 
-  def move(board, index, current_player = "X")
-    board[index] = current_player
-  end
+def move(board, selection, char="X")
+  board = board[selection] = char
+end
