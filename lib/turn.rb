4bd656bf5c8f90b30951f8c0b263876838160426
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-09-14 14:59:30 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,33 @@
+def display_board(board)
+  puts " #{ board[0] } | #{ board[1] } | #{ board[2] } "
+  puts "-----------"
+  puts " #{ board[3] } | #{ board[4] } | #{ board[5] } "
+  puts "-----------"
+  puts " #{ board[6] } | #{ board[7] } | #{ board[8] } "
+end
+
+def valid_move?(board, index)
+  position_taken?(board, index) == false && index.between?(0,8)
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def input_to_index(user_input)
+  return user_input.to_i - 1
+end
+
+def move(array, index, value = "X")
+  array[index] = value
+end
+
+def turn(board, index)
+  puts "Please enter 1-9:"
+  index = input_to_index(gets.strip)
+  if valid_move?(board, index)
+    move(array, index, value = "X")
+  else
+    puts "Please enter 1-9:"
+  end
+end
