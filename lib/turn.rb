9681ef349d040c65a12e9e291157cc8072e38d8f
Tom Sharkey <tbsharkey@gmail.com>
2019-07-21 07:09:59 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,14 +1,3 @@
-def turn(board)
-puts "Please enter 1-9:"
-input=gets
-index = input_to_index(input)
-if valid_move?(board, index) == false
-  turn(board)
-else move(board, index)
-display_board(board)
-end
-end
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -17,21 +6,27 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?(board,index) != true
-      true
-   else
-    false
-   end
-  end
-
 def input_to_index(input)
-index=input.to_i-1
+  index=input.to_i-1
 end
 
-def move(board, index, value="X")
-board[index]=value
-return board
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board,index) != true
+    true
+  else
+    false
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+  if valid_move?(board, index) == false
+    turn(board)
+  else move(board, index)
+    display_board(board)
+  end
 end
 
 def position_taken?(board, index)
@@ -42,4 +37,8 @@ def position_taken?(board, index)
   else
     false
   end
+end
+
+def move(board, index, player="X")
+  board[index]=player
 end
