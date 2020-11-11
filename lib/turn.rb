1d103fa834cc2103ec45d30a71fc77121ba71131
andy-aguilar <aaguil3@gmail.com>
2020-11-10 18:05:39 -0600

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+require "pry"
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+    return user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+    #binding.pry
+    if board[index] == "X" || board[index] == "O" || index < 0 || index > 8
+        return false
+    end
+    true
+end
+
+def move(board, index, token)
+    board[index] = token
+end
+
+def turn(board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(board, index)
+        move(board, index, "X")
+    else
+        turn(board)
+    end
+    display_board(board)
+end
